879a8a40db58b9c22f50b9785e6cbffe01ec28d8
Alexander V. Basyrov <basyrov.av@gmail.com>
2016-07-17 04:43:48 +0300

    test 2

--- init.sql
+++ init.sql
@@ -1,4 +1,5 @@
 CREATE USER 'napopravku'@'localhost' IDENTIFIED BY 'password12345';
 CREATE DATABASE `napopravku_db` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
 GRANT SELECT,DELETE,INSERT,UPDATE ON napopravku_db.* TO 'napopravku'@'localhost';
 FLUSH PRIVILEGES;
